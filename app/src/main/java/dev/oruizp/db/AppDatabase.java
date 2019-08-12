3050b35f55170ccab1ec32059ee2d43082938300
oruizp <oruizp@live.com>
2019-08-12 17:06:51 -0500

    Room database class added

---
+++ app/src/main/java/dev/oruizp/db/AppDatabase.java
@@ -0,0 +1,8 @@
+package dev.oruizp.db;
+
+import androidx.room.RoomDatabase;
+
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+}
