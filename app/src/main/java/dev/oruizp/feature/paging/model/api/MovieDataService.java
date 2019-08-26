965d6002b076123bdfba15d5dc1d756a4786006a
oruizp <oruizp@live.com>
2019-08-26 18:22:02 -0500

    Paging first approach added

---
+++ app/src/main/java/dev/oruizp/feature/paging/model/api/MovieDataService.java
@@ -0,0 +1,16 @@
+package dev.oruizp.feature.paging.model.api;
+
+import dev.oruizp.feature.paging.model.api.model.MovieDBResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieDataService {
+
+    @GET("movie/popular")
+    Call<MovieDBResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/popular")
+    Call<MovieDBResponse> getPopularMoviesWithPaging(@Query("api_key") String apiKey, @Query("page") long page);
+
+}
---
+++ app/src/main/java/dev/oruizp/feature/paging/model/api/RestServiceFactory.java
@@ -0,0 +1,29 @@
+package dev.oruizp.feature.paging.model.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RestServiceFactory {
+
+    /**
+     * Creates a retrofit service from an arbitrary class (clazz)
+     *
+     * @param clazz    Java interface of the retrofit service
+     * @param endPoint REST endpoint url
+     * @return retrofit service with defined endpoint
+     */
+    public static <T> T createRetrofitService(final Class<T> clazz, final String endPoint) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.level(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        final Retrofit restAdapter = new Retrofit.Builder()
+                .baseUrl(endPoint)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return restAdapter.create(clazz);
+    }
+}
